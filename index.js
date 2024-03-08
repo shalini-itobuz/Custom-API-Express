a734685840f97194b208122f74a2d132b8f7e1e0
shalini-itobuz <shalini@itobuz.com>
2024-03-08 10:20:56 +0530

    version 2.0

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/weather/:location', (req, res) => {
   const locationData = locations.find(loc => loc.name.toLowerCase() === locationName);
 
   if (locationData) {
-    res.status(200).json({ data: locationData , status: 'successful', message: 'Weather data found'});
+    res.status(200).json({ data: locationData, status: 'successful', message: 'Weather data found' });
   } else {
     res.status(404).json({ status: 'error', message: 'Weather data not found for the specified location' });
   }
@@ -20,17 +20,17 @@ app.get('/weather/:location', (req, res) => {
 // Route post
 app.post('/addweather', (req, res) => {
   const newLocationData = req.body;
-  console.log(newLocationData);
+  console.log("Data added successfully");
   if (!newLocationData.name || !newLocationData.region || !newLocationData.country || !newLocationData.temp_c || !newLocationData.temp_f || !newLocationData.feelslike_c || !newLocationData.feelslike_f) {
     return res.status(400).json({ status: 'error', message: 'All fields are required' });
   }
   locations.push(newLocationData);
-  res.status(201).json({ data: newLocationData, status: 'successful', message: 'Weather data added successfully' });
+  res.status(201).json({ data: newLocationData, status: 201, message: 'Weather data added successfully' });
 });
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).json({ status: 'error', message: 'Internal server error' });
+  res.status(500).json({ status: 500, message: 'Internal server error' });
 });
 
 // server
